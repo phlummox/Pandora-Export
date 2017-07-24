cd3d2528baff2a74df21e6c89ec0698eee1b9d26
phlummox <none@none>
2017-07-24 19:31:23 +0800

    try python 3.4 in appveyor
    --HG--
    branch : winXP-sslverify-disable

--- pandora/pandora.py
+++ pandora/pandora.py
@@ -27,8 +27,8 @@ import logging
 import time
 import urllib.request, urllib.parse, urllib.error
 import codecs
-import backports.ssl as ssl
-#import ssl
+#import backports.ssl as ssl
+import ssl
 from enum import IntEnum
 
 from . import data
